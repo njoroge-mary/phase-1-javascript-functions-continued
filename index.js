84e569c0842e727d2d68d1347c7da5bbb0a87f82
marynjoroge <wangarinjorogekenya@gmail.com>
2024-01-17 02:29:04 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // code your solution here
+function saturdayFun(activity ='roller-skate'){
+    if (activity !== 'roller-skate'){
+        return "This Saturday, I want to bathe my dog!"
+    }else if (activity === 'roller-skate'){ 
+        return "This Saturday, I want to roller-skate!"
+    }else{
+        return "This Saturday, I want to roller-skate!"
+    }
+}
+function mondayWork(activity = 'go to the office'){
+    if (activity !== 'go to the office'){
+        return "This Monday, I will work from home."
+    }else if (activity === 'go to the office'){
+        return "This Monday, I will go to the office."
+    }else {
+        return "This Monday, I will go to the office."
+    }
+}
+function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+        return `You are ${flair}${adjective}${flair}!`;
+    };
+}
+
